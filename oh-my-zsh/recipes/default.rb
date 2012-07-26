dce8cbcb4dee8aa22b7980b333031dd001721167
Tower He <towerhe@gmail.com>
2012-07-26 15:16:29 +0800

    Installed for vagrant only

--- oh-my-zsh/recipes/default.rb
+++ oh-my-zsh/recipes/default.rb
@@ -9,25 +9,21 @@
 include_recipe "git"
 include_recipe "zsh"
 
-search( :users, "shell:*zsh" ).each do |u|
-  user_id = u["id"]
+user_id = 'vagrant'
 
-  git "/home/#{user_id}/.oh-my-zsh" do
-    repository "https://github.com/robbyrussell/oh-my-zsh.git"
-    reference "master"
-    user user_id
-    group user_id
-    action :checkout
-    not_if "test -d /home/#{user_id}/.oh-my-zsh"
-  end
+git "/home/#{user_id}/.oh-my-zsh" do
+  repository "https://github.com/robbyrussell/oh-my-zsh.git"
+  reference "master"
+  user user_id
+  group user_id
+  action :checkout
+  not_if "test -d /home/#{user_id}/.oh-my-zsh"
+end
 
-  theme = data_bag_item( "users", user_id )["oh-my-zsh-theme"]
-
-  template "/home/#{user_id}/.zshrc" do
-    source "zshrc.erb"
-    owner user_id
-    group user_id
-    variables( :theme => ( theme || node[:ohmyzsh][:theme] ))
-    action :create_if_missing
-  end
+template "/home/#{user_id}/.zshrc" do
+  source "zshrc.erb"
+  owner user_id
+  group user_id
+  variables(:theme => node[:ohmyzsh][:theme])
+  action :create_if_missing
 end
