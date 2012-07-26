1e7fb56c52067e8cfb59ecc75c7c0c193f31ea26
Tower He <towerhe@gmail.com>
2012-07-26 15:54:36 +0800

    Cookbook for installing and configuring janus

---
+++ janus/attributes/default.rb
@@ -0,0 +1,25 @@
+# Cookbook Name:: janus
+# Attributes:: default
+#
+# #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Required packages for Janus
+default['janus']['required_packages'] = %w{ curl vim vim-nox rake
+                                            ruby-dev exuberant-ctags git-core }
+
+# Set working directory and current user of node for installation.
+# I think databags are supposed to be used instead
+default['janus']['user']    = "vagrant"
+default['janus']['dir']     = "/home/#{node['janus']['user']}"
