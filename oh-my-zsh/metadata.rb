0e9d80026def33a87c5fdb1e00a3569b7e0bc600
Tower He <towerhe@gmail.com>
2012-07-26 15:48:09 +0800

    Changed the license

--- oh-my-zsh/metadata.rb
+++ oh-my-zsh/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Beijing Menglifang Network Science and Technology Co.,Ltd."
 maintainer_email "master@menglifang.org"
-license          "MIT"
+license          "Apache License, Version 2.0"
 description      "Installs/Configures oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
