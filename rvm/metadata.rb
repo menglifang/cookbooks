6b444026e11640d489f402dcfb5bdc52afb9ac21
Tower He <towerhe@gmail.com>
2012-09-11 15:21:38 +0800

    Specified the depends

--- rvm/metadata.rb
+++ rvm/metadata.rb
@@ -4,3 +4,11 @@ license          "MIT"
 description      "Installs/Configures rvm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+recipe "rvm", "Install RVM and Ruby"
+
+depends "git"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
