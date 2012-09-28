731ee4d2d52362e3736a0f46f1a3785ff15774f0
Tower He <towerhe@gmail.com>
2012-09-28 18:54:26 +0800

    Added a cookbook to install mri ruby

---
+++ ruby/attributes/default.rb
@@ -0,0 +1,5 @@
+default['ruby']['mri']['required_packages'] = %w{ build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config }
+
+default['ruby']['interpreter'] = 'mri'
+default['ruby']['version'] = '1.9.3'
+
