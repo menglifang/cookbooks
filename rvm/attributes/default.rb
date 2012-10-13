6234ff34b2357dc9e34d7eb6d65c3286df8dfcfe
Tower He <towerhe@gmail.com>
2012-10-13 18:04:15 +0800

    current_user is set to the login user

--- rvm/attributes/default.rb
+++ rvm/attributes/default.rb
@@ -1,4 +1,2 @@
-current_user = fix_encoding(Etc.getpwuid.name)
-
-set['rvm']['user']    = current_user
-set['rvm']['dir']     = current_user == 'root' ? "/usr/local/rvm" : "/home/#{current_user}/.rvm"
+set['rvm']['user']    = node.current_user
+set['rvm']['dir']     = "/home/#{node.current_user}/.rvm"
