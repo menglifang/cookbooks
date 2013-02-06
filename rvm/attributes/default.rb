695c73b36e2389e969717908430a9357f47eb88b
Tower He <towerhe@gmail.com>
2013-02-06 10:16:01 +0800

    Set the default values by current user's name

--- rvm/attributes/default.rb
+++ rvm/attributes/default.rb
@@ -1,3 +1,3 @@
-default['rvm']['global']    = false
 default['rvm']['user']      = node.current_user
-default['rvm']['dir']       = "/home/#{node.current_user}/.rvm"
+default['rvm']['global']    = node.current_user == 'root'
+default['rvm']['dir']       = default['rvm']['global'] ? "/usr/local/rvm" : "/home/#{node.current_user}/.rvm"
